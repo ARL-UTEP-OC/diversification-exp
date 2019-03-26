d95ad63dbe2b8e77e9e6b9baf3b6dca1f6991820
raistlinJ <raistlinJ@users.noreply.github.com>
2019-03-25 21:37:34 -0600

    Keyphrase-based search/graphing added

--- graph_exp.py
+++ graph_exp.py
@@ -6,11 +6,10 @@ from os.path import isfile, join
 from subprocess import Popen
 import sys
 import shlex
-#import plotly.plotly as py
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 import plotly.graph_objs as go
 
-def filedata_to_dict(foldername):
+def filedata_to_dict(foldername, keyphrases):
 	logging.debug("filedata_to_dict() instantiated")
 	bin_data = {}
 	exp_files = [f for f in listdir(foldername) if isfile(join(foldername, f)) and f.endswith(".perf")]
@@ -21,37 +20,40 @@ def filedata_to_dict(foldername):
 		binname = splitfilename[0]
 		if binname not in bin_data:
 			bin_data[binname] = {}
+			
 		#get file data
-		bin_data[binname][filename] = getfiledata(join(foldername,filename), "seconds time elapsed")
+		bin_data[binname][filename] = getfiledata(join(foldername,filename), keyphrases)
 	logging.debug("filedata_to_dict() completed")
 	return bin_data
 
-def getfiledata(filepath, keyphrase):
+def getfiledata(filepath, keyphrases):
 	logging.debug("getfiledata() instantiated")
 	logging.debug("getfiledata(): looking in file: " + filepath)
 	answer = {}
-	found = 0
-
+	found = {}
+	for keyphrase in keyphrases:
+		found[keyphrase] = 0
 	try:
 		with open(filepath) as f:
 			#get the parameters we want
 			for line in f:
 				#logging.debug("getfiledata(): looking for string in line: " + line)
 				#get time elapsed data
-				if keyphrase in line:
-					found = 1
-					logging.debug("getfiledata(): string found: " + line.split()[0])
-					answer['time'] = float(line.split()[0])
-				#add more fields as needed here
-		if found == 0:
-			logging.error("String " + "seconds time elapsed" + " not found in file: " + filepath)
+				for keyphrase in keyphrases:
+					if keyphrase in line:
+						found[keyphrase] = 1
+						logging.debug("getfiledata(): string found: " + line.split()[0])
+						answer[keyphrase] = float(line.split()[0])
+				
+		if not all( x==1 for x in found.values()):
+			logging.error("String " + keyphrase + " not found in file: " + filepath)
 			exit() 
 	except Exception as e:
 		logging.error("Error when reading file: " + e)	
 
 	return answer
 	
-def graph_gen_time(bin_data, stat_name, title):
+def graph_gen(bin_data, stat_name, title):
 	logging.debug("graph_gen_time() instantiated")
 	data = []
 	for binname, configs in bin_data.iteritems():
@@ -81,5 +83,7 @@ if __name__ == "__main__":
 		logging.error("Usage: graph_exp.py <perf-file-directory>")
 		exit()
 	foldername = sys.argv[1]
-	bindata = filedata_to_dict(foldername)
-	graph_gen_time(bindata, stat_name="time", title="Time Elapsed")
+	keyphrases = ["seconds time elapsed", "cpu-clock"]
+	bindata = filedata_to_dict(foldername, keyphrases)
+	#graph_gen(bindata, stat_name="seconds time elapsed", title="Time Elapsed")
+	graph_gen(bindata, stat_name="cpu-clock", title="CPU Clock")
